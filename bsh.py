15006913d3980d1e3c3e285af1e66af6c2e2ea31
cinarkagan <68916102+cinarkagan@users.noreply.github.com>
2022-09-29 17:55:44 +0300

    Add files via upload

---
+++ bsh.py
@@ -0,0 +1,44 @@
+def bsh(filename):
+    f = open(filename,'r')
+    path = None
+    for line in f:
+        if line.startswith('#!'):
+            path = line.replace('#!','').strip()
+            mode = 'non'
+            fp = open(path+'/py.py','w')
+            fb = open(path+'/bash.sh','w')
+            fv = open(path+'/.bin','w')
+            fr = open(path+'/run.sh','w')
+
+            #run
+            fr.write('bash bash.sh'+'\n')
+            fr.write('python py.py')
+
+            #python
+            fr.close()
+            fp.write('bsh = {}'+'\n')
+            fp.write('def getf(var):'+'\n')
+            fp.write('  '+'f = open(".bin","r")'+'\n')
+            fp.write('  '+'for line in f:'+'\n')
+            fp.write('      '+'line = line.strip()'+'\n')
+            fp.write('      '+'if line.startswith(var+"="):'+'\n')
+            fp.write('          '+'bsh[var]=line.replace(var+"=","")'+'\n')
+            #bash
+            fb.write('bshbin=0'+'\n')
+            fb.write('function exportf() {'+'\n')
+            fb.write('    echo "$1=$bshbin" >> .bin'+'\n')
+            fb.write('}'+'\n')
+            #parsing
+            for line1 in f:
+                if line1.startswith('-b'):
+                    mode = 'bash'
+                elif line1.startswith('-p'):
+                    mode = 'python'
+                else:
+                    if mode == 'bash':
+                        fb.write(line1+'\n')
+                    elif mode == 'python':
+                        fp.write(line1+'\n')
+
+bsh('example.d')
+                
